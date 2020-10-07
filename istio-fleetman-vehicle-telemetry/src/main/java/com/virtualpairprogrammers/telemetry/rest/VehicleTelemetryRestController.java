dca244d064111c33c28430721899514b361f8233
FabioSalas <fabio.salas@globant.com>
2020-10-07 00:41:24 -0500

    Actualizado: Migrado componente 'vehicle-telemetry' a spring boot 2.3.4

--- istio-fleetman-vehicle-telemetry/src/main/java/com/virtualpairprogrammers/telemetry/rest/VehicleTelemetryRestController.java
+++ istio-fleetman-vehicle-telemetry/src/main/java/com/virtualpairprogrammers/telemetry/rest/VehicleTelemetryRestController.java
@@ -1,32 +1,24 @@
 package com.virtualpairprogrammers.telemetry.rest;
 
-import java.math.BigDecimal;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.virtualpairprogrammers.telemetry.service.VehiclePosition;
 import com.virtualpairprogrammers.telemetry.service.VehicleTelemetryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
 @RestController
-public class VehicleTelemetryRestController 
-{
-	@Autowired
-	private VehicleTelemetryService service;
-	
-	@RequestMapping(method=RequestMethod.POST, value="/vehicles/")
-	public void updateData(@RequestBody VehiclePosition data)
-	{
-		this.service.updateData(data);
-	}
+public class VehicleTelemetryRestController {
+    private final VehicleTelemetryService service;
 
-	@RequestMapping(method=RequestMethod.GET, value="/vehicles/{vehicleName}")
-	public BigDecimal getSpeedFor(@PathVariable("vehicleName") String vehicleName)
-	{
-		return this.service.getSpeedFor(vehicleName);
-	}
+    @RequestMapping(method = RequestMethod.POST, value = "/vehicles")
+    public void updateData(@RequestBody VehiclePosition data) {
+        this.service.updateData(data);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/vehicles/{vehicleName}")
+    public BigDecimal getSpeedFor(@PathVariable("vehicleName") String vehicleName) {
+        return this.service.getSpeedFor(vehicleName);
+    }
 }
--- istio-fleetman-vehicle-telemetry/src/main/java/com/virtualpairprogrammers/telemetry/service/VehiclePosition.java
+++ istio-fleetman-vehicle-telemetry/src/main/java/com/virtualpairprogrammers/telemetry/service/VehiclePosition.java
@@ -1,97 +1,92 @@
 package com.virtualpairprogrammers.telemetry.service;
 
-import java.math.BigDecimal;
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
+import java.math.BigDecimal;
+import java.util.Date;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-public class VehiclePosition implements Comparable<VehiclePosition>
-{
-	private String name;
-	private BigDecimal lat;
-	private BigDecimal lng;
-	
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss.SSSZ", timezone="UTC")
-	private Date timestamp;
-	private BigDecimal speed;
-	
-	VehiclePosition() {}
-	
-	VehiclePosition(String name, BigDecimal lat, BigDecimal lng, Date timestamp, BigDecimal speed) {
-		this.name = name;
-		this.lat = lat;
-		this.lng = lng;
-		this.timestamp = timestamp;
-		this.speed = speed;
-	}
+public class VehiclePosition implements Comparable<VehiclePosition> {
+    private String name;
+    private BigDecimal lat;
+    private BigDecimal lng;
 
-	@Override
-	public int compareTo(VehiclePosition o) 
-	{
-		return o.timestamp.compareTo(this.timestamp);
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((timestamp == null) ? 0 : timestamp.hashCode());
-		return result;
-	}
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ", timezone = "UTC")
+    private Date timestamp;
+    private BigDecimal speed;
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		VehiclePosition other = (VehiclePosition) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (timestamp == null) {
-			if (other.timestamp != null)
-				return false;
-		} else if (!timestamp.equals(other.timestamp))
-			return false;
-		return true;
-	}
+    VehiclePosition() {
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    VehiclePosition(String name, BigDecimal lat, BigDecimal lng, Date timestamp, BigDecimal speed) {
+        this.name = name;
+        this.lat = lat;
+        this.lng = lng;
+        this.timestamp = timestamp;
+        this.speed = speed;
+    }
 
-	public BigDecimal getLat() {
-		return this.lat;
-	}
+    @Override
+    public int compareTo(VehiclePosition o) {
+        return o.timestamp.compareTo(this.timestamp);
+    }
 
-	public BigDecimal getLng() {
-		return this.lng;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((timestamp == null) ? 0 : timestamp.hashCode());
+        return result;
+    }
 
-	public Date getTimestamp() {
-		return this.timestamp;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        VehiclePosition other = (VehiclePosition) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (timestamp == null) {
+            return other.timestamp == null;
+        } else return timestamp.equals(other.timestamp);
+    }
 
-	public BigDecimal getSpeed() {
-		return this.speed;
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	@Override
-	public String toString() {
-		return "VehiclePosition [name=" + name + ", lat=" + lat + ", lng=" + lng + ", timestamp="
-				+ timestamp + ", speed=" + speed + "]";
-	}
+    public BigDecimal getLat() {
+        return this.lat;
+    }
+
+    public BigDecimal getLng() {
+        return this.lng;
+    }
+
+    public Date getTimestamp() {
+        return this.timestamp;
+    }
+
+    public BigDecimal getSpeed() {
+        return this.speed;
+    }
+
+    @Override
+    public String toString() {
+        return "VehiclePosition [name=" + name + ", lat=" + lat + ", lng=" + lng + ", timestamp="
+                + timestamp + ", speed=" + speed + "]";
+    }
 
 }
